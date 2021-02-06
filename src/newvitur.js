7112ff509bc9ddb0e3a1941cf5d74382343bfb27
KingIdBot <77149298+KingIdBot@users.noreply.github.com>
2021-02-06 15:48:04 +0800

    Add files via upload

---
+++ src/newvitur.js
@@ -0,0 +1,12 @@
+const newvitur = (prefix) => { 
+	return `
+╔══✪〘 UPDATE 〙✪══
+║
+║
+╠➥ *${prefix}happymod <nama apk>*
+╠➥ *${prefix}modroid <nama apk>*
+╠➥ *${prefix}play2 <nama lagu>*
+║
+╚═〘 MR KING BOT 〙`
+}
+exports.newvitur = newvitur
